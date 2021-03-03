8733602dcc3c44b560397f265ba3dbe41fc69052
Lateralus138 <Lateralus138@users.noreply.github.com>
2021-03-03 22:53:55 +0000

    Built Linux Binary

---
+++ target/linux/release/deps/blackhosts-6531b6daf5e4bf0f.d
@@ -0,0 +1,5 @@
+/home/runner/work/blackhosts/blackhosts/target/linux/release/deps/blackhosts-6531b6daf5e4bf0f: src/main.rs
+
+/home/runner/work/blackhosts/blackhosts/target/linux/release/deps/blackhosts-6531b6daf5e4bf0f.d: src/main.rs
+
+src/main.rs:
--- target/linux/release/deps/blackhosts-e1a59a61c2aee4ca.d
+++
@@ -1,5 +0,0 @@
-/home/runner/work/blackhosts/blackhosts/target/linux/release/deps/blackhosts-e1a59a61c2aee4ca: src/main.rs
-
-/home/runner/work/blackhosts/blackhosts/target/linux/release/deps/blackhosts-e1a59a61c2aee4ca.d: src/main.rs
-
-src/main.rs:
